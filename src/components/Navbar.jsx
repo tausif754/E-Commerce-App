657843427ecc3330be483dedae4bf6066af8b1a7
Tausif Ahmad <tausifahmad754@gmail.com>
2024-09-01 03:47:10 +0530

    Our navigationabar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, NavLink, Link } from "react-router-dom";
 import { assets } from "../assets/assets";
 
 const Navbar = () => {
+  const [visible, setVisible] = useState(false);
   return (
     <Router>
       <div className="flex items-center justify-between py-5 font-medium">
@@ -57,6 +58,64 @@ const Navbar = () => {
               10
             </p>
           </Link>
+
+          <img
+            onClick={() => setVisible(true)}
+            src={assets.menu_icon}
+            alt=""
+            className="w-5 cursor-pointer sm:hidden"
+          />
+        </div>
+        {/* Sidebar menu for small Screen */}
+        <div
+          className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${
+            visible ? "w-full" : "w-0"
+          }`}
+        >
+          <div className="flex flex-col text-gray-600">
+            <div
+              onClick={() => setVisible(false)}
+              className="flex items-center gap-4 p-3 cursor-pointer"
+            >
+              <img
+                src={assets.dropdown_icon}
+                alt=""
+                className="h-4 rotate-180"
+              />
+              <p>Back</p>
+            </div>
+            <NavLink
+              onClick={() => setVisible(false)}
+              className="py-2 pl-6 border"
+              to="./"
+            >
+              {" "}
+              HOME
+            </NavLink>
+            <NavLink
+              onClick={() => setVisible(false)}
+              className="py-2 pl-6 border"
+              to="./collections"
+            >
+              {" "}
+              COLLECTIONS
+            </NavLink>
+            <NavLink
+              onClick={() => setVisible(false)}
+              className="py-2 pl-6 border"
+              to="./about"
+            >
+              {" "}
+              ABOUT
+            </NavLink>
+            <NavLink
+              onClick={() => setVisible(false)}
+              className="py-2 pl-6 border"
+              to=".contact"
+            >
+              CONTACT{" "}
+            </NavLink>
+          </div>
         </div>
       </div>
     </Router>
