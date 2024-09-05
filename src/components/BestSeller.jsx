e4bacafb51f4d9ac95015f95b9bdc7204593e0ae
Tausif Ahmad <tausifahmad754@gmail.com>
2024-09-06 01:37:19 +0530

    Best seller section added

---
+++ src/components/BestSeller.jsx
@@ -0,0 +1,40 @@
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Titles from "./Titles";
+import ProductItem from "./ProductItem";
+
+const BestSeller = () => {
+  const { products } = useContext(ShopContext);
+  const [bestSeller, setBestSeller] = useState([]);
+  useEffect(() => {
+    // console.log(products);
+    const bestProduct = products.filter((item) => item.bestseller);
+    setBestSeller(bestProduct.slice(0, 5));
+  }, [products]);
+  return (
+    <div className="my-10">
+      <div className="text-center text-3xl py-8">
+        <Titles text1={"BEST"} text2={"SELLER"} />
+
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam ipsa
+          ratione, ea ipsam voluptates hic?
+        </p>
+      </div>
+
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y6">
+        {bestSeller.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            name={item.name}
+            image={item.image}
+            price={item.price}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BestSeller;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../components/Hero";
 import LatestCollections from "../components/LatestCollections";
+import BestSeller from "../components/BestSeller";
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <LatestCollections />
+      <BestSeller />
     </div>
   );
 };
