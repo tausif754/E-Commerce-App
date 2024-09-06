4d543c4782ca2e0ff40c8dcce75a0e803a30830c
Tausif Ahmad <tausifahmad754@gmail.com>
2024-09-07 02:30:18 +0530

    Filter functionality added on collections component

--- src/pages/Collections.jsx
+++ src/pages/Collections.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
 
 const Collections = () => {
-  return <div>Collections</div>;
+  const { products } = useContext(ShopContext);
+  return (
+    <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
+      {/* Filter options */}
+      <div className="min-w-60">
+        <p className="my-2 text-xl flex items-center cursor-pointer gap-2">
+          FILTERS
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Collections;
