1bdca5db9c4ebd3d8b76fdb76dbd81c49de68de3
Tausif Ahmad <tausifahmad754@gmail.com>
2024-09-09 00:59:54 +0530

    Category filter added

--- src/pages/Collections.jsx
+++ src/pages/Collections.jsx
@@ -1,8 +1,9 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 
 const Collections = () => {
   const { products } = useContext(ShopContext);
+  const [showFilter, setShowFilter] = useState(false);
   return (
     <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
       {/* Filter options */}
@@ -10,6 +11,25 @@ const Collections = () => {
         <p className="my-2 text-xl flex items-center cursor-pointer gap-2">
           FILTERS
         </p>
+        {/* Category Filter */}
+        <div
+          className={`border border-gray-300 pl-5 py-3 mt-6 ${
+            showFilter ? "" : "hidden"
+          } sm:block`}
+        >
+          <p className="mb-3 text-sm font-medium">CATEGORIES</p>
+          <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+            <p className="flex gap-2">
+              <input className="w-3" type="checkbox" value={"Men"} /> Men
+            </p>
+            <p className="flex gap-2">
+              <input className="w-3" type="checkbox" value={"Women"} /> Women
+            </p>
+            <p className="flex gap-2">
+              <input className="w-3" type="checkbox" value={"Kids"} /> Kids
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
