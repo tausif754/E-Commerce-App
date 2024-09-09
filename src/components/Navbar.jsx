bfdd4c09db7e446a1c2e7c0ed9fc81a55b568d37
Tausif Ahmad <tausifahmad754@gmail.com>
2024-09-09 18:17:13 +0530

    Our Searchbar component is ready

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 // import { BrowserRouter as Router, NavLink, Link } from "react-router-dom";
 import { NavLink, Link } from "react-router-dom";
 import { assets } from "../assets/assets";
+import { ShopContext } from "../context/ShopContext";
 
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
+
+  const { setShowSearch } = useContext(ShopContext);
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       <Link to="/">
@@ -30,7 +33,12 @@ const Navbar = () => {
       </ul>
 
       <div className="flex items-center gap-6">
-        <img src={assets.search_icon} alt="" className="w-5 cursor-pointer" />
+        <img
+          onClick={() => setShowSearch(true)}
+          src={assets.search_icon}
+          alt=""
+          className="w-5 cursor-pointer"
+        />
 
         <div className="group relative">
           <img
