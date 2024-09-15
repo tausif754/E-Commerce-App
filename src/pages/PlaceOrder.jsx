bf435a258afc81830321aef8061b3681512a8101
Tausif Ahmad <tausifahmad754@gmail.com>
2024-09-15 17:41:01 +0530

    committed

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Titles from "../components/Titles";
 
 const PlaceOrder = () => {
-  return <div>PlaceOrder</div>;
+  const { products, currency, cartItems } = useContext(ShopContext);
+
+  const [cartData, setCartData] = useState([]);
+
+  useEffect(() => {
+    const tempData = [];
+    for (const items in cartItems) {
+      for (const item in cartItems[item]) {
+        if (cartItems[items][item] > 0) {
+          tempData.push({
+            _id: items,
+            size: item,
+            quantity: cartItems[items][item],
+          });
+        }
+      }
+    }
+    // console.log(tempData);
+    setCartData(tempData);
+  }, [cartItems]);
+  return (
+    <div className="border-t pt-14">
+      <div className="text-2xl mb-3">
+        <Titles text1={"YOUR"} text2={"CART"} />
+      </div>
+      <div>
+        {cartData.map((item, index) => {
+          const productData = products.find(
+            (product) => product._id === item._id
+          );
+
+          return (
+            <div
+              key={index}
+              className="py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4"
+            >
+              <div className="felx items-start gap-6">
+                <img
+                  className="w-16 sm:w-20 "
+                  src={productData.image[0]}
+                  alt=""
+                />
+                <div>
+                  <p className="text-xs sm:text-lg font-medium">
+                    {productData.name}
+                  </p>
+                  <div className="flex items-center gap-5 mt-2">
+                    <p>
+                      {currency}
+                      {productData.price}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default PlaceOrder;
