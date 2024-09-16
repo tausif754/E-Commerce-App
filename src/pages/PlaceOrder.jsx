3f23644ed498a92987c2ff8710018e7a7dde42d3
Tausif Ahmad <tausifahmad754@gmail.com>
2024-09-16 10:46:50 +0530

    Our place order page completed

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -1,65 +1,131 @@
 import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import Titles from "../components/Titles";
+import CartTotal from "../components/CartTotal";
+import { assets } from "../assets/assets";
 
 const PlaceOrder = () => {
   const { products, currency, cartItems } = useContext(ShopContext);
 
-  const [cartData, setCartData] = useState([]);
+  const [method, setMethod] = useState("cod");
+  const { navigate } = useContext(ShopContext);
 
-  useEffect(() => {
-    const tempData = [];
-    for (const items in cartItems) {
-      for (const item in cartItems[item]) {
-        if (cartItems[items][item] > 0) {
-          tempData.push({
-            _id: items,
-            size: item,
-            quantity: cartItems[items][item],
-          });
-        }
-      }
-    }
-    // console.log(tempData);
-    setCartData(tempData);
-  }, [cartItems]);
   return (
-    <div className="border-t pt-14">
-      <div className="text-2xl mb-3">
-        <Titles text1={"YOUR"} text2={"CART"} />
+    <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t">
+      {/* -------------Left Side------------ */}
+      <div className="flex flex-col gap-4 w-full sm:max-w-[480px]">
+        <div className="text-xl sm:text-2xl my-3">
+          <Titles text1={"DELIVERY"} text2={"INFORMATION"} />
+        </div>
+        <div className="flex gap-3">
+          <input
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            type="text"
+            placeholder="First name"
+          />
+          <input
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            type="text"
+            placeholder="Last name"
+          />
+        </div>
+        <input
+          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          type="email"
+          placeholder="Email address"
+        />
+        <input
+          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          type="text"
+          placeholder="Street"
+        />
+        <div className="flex gap-3">
+          <input
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            type="text"
+            placeholder="City"
+          />
+          <input
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            type="text"
+            placeholder="State"
+          />
+        </div>
+        <div className="flex gap-3">
+          <input
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            type="number"
+            placeholder="Zipcode"
+          />
+          <input
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            type="text"
+            placeholder="Country"
+          />
+        </div>
+        <input
+          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          type="number"
+          placeholder="Phone"
+        />
       </div>
-      <div>
-        {cartData.map((item, index) => {
-          const productData = products.find(
-            (product) => product._id === item._id
-          );
+      {/* --------------Right Side---------- */}
 
-          return (
-            <div
-              key={index}
-              className="py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4"
-            >
-              <div className="felx items-start gap-6">
-                <img
-                  className="w-16 sm:w-20 "
-                  src={productData.image[0]}
-                  alt=""
-                />
-                <div>
-                  <p className="text-xs sm:text-lg font-medium">
-                    {productData.name}
-                  </p>
-                  <div className="flex items-center gap-5 mt-2">
-                    <p>
-                      {currency}
-                      {productData.price}
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          );
-        })}
+      <div className="mt-8">
+        <div className="mt-8 min-w-80">
+          <CartTotal />
+        </div>
+
+        <div className="mt-12">
+          <Titles text1={"PAYMENT"} text2={"METHOD"} />
+        </div>
+        {/* ------------- Payment Method Selection--------- */}
+        <div className="flex gap-3 flex-col lg:flex-row">
+          <div
+            onClick={() => setMethod("strip")}
+            className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
+          >
+            <p
+              className={`min-w-3.5 h-3.5 border rounded-full ${
+                method === "strip" ? "bg-green-400" : ""
+              }`}
+            ></p>
+            <img className="h-5 mx-4" src={assets.stripe_logo} alt="" />
+          </div>
+          <div
+            onClick={() => setMethod("razorpay")}
+            className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
+          >
+            <p
+              className={`min-w-3.5 h-3.5 border rounded-full ${
+                method === "razorpay" ? "bg-green-400" : ""
+              }`}
+            ></p>
+            <img className="h-5 mx-4" src={assets.razorpay_logo} alt="" />
+          </div>
+          <div
+            onClick={() => setMethod("cod")}
+            className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
+          >
+            <p
+              className={`min-w-3.5 h-3.5 border rounded-full ${
+                method === "cod" ? "bg-green-400" : ""
+              }`}
+            ></p>
+            <p className="text-gray-500 text-sm font-medium mx-4">
+              CASH ON DELIVERY
+            </p>
+          </div>
+        </div>
+        {/* ------- Button---------- */}
+        <div className="w-full text-end mt-8">
+          <button
+            onClick={() => navigate("/orders")}
+            className="bg-black text-white px-16 py-3 text-sm"
+          >
+            PLACE ORDER
+          </button>
+        </div>
       </div>
     </div>
   );
